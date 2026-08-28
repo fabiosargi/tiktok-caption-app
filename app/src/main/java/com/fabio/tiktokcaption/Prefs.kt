b7e9501d04f5dbcd71525bb8a91a576c39a7c5ff
fabiosargi <fabiosargi00@gmail.com>
2026-08-28 03:11:32 -0300

    Create Prefs.kt

---
+++ app/src/main/java/com/fabio/tiktokcaption/Prefs.kt
@@ -0,0 +1,8 @@
+package com.fabio.tiktokcaption
+
+/** Nomes centralizados de SharedPreferences, compartilhados entre a Activity e o AccessibilityService. */
+object Prefs {
+      const val NAME = "app_prefs"
+      const val KEY_API_KEY = "gemini_api_key"
+      const val KEY_PENDING_CAPTION = "pending_caption"
+}
