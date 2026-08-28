62248ae40289eb1ffb342afa1fb27c4637d5f672
fabiosargi <fabiosargi00@gmail.com>
2026-08-28 19:33:31 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fabio.tiktokcaption"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
